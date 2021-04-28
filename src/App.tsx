d7f0ad298951e8686bb5e346f95a59c56cf9b2fe
zohar <injapanthree@gmail.com>
2021-04-27 20:51:36 -0400

    basic screen don

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import {} from "@chakra-ui/react";
+import {
+	Button,
+	Stack,
+	ChakraProvider,
+	Flex,
+	Center,
+	Spacer,
+	Text,
+	Box,
+} from "@chakra-ui/react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import "./App.css";
+
+const App = () => {
+	const send_motor_command = (motor_num: number, action: string) => {
+		console.log("sending " + action + " for motor " + String(motor_num));
+		const request_options = {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({
+				type: "motor",
+				motor: motor_num,
+				action: action,
+			}),
+		};
+
+		fetch("http://10.0.0.160:3000", request_options).then((response) =>
+			// console.log(response.json())
+			console.log("test")
+		);
+	};
+
+	const motors = [0, 1, 2, 3, 4, 5];
+
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<ChakraProvider>
+					<Stack spacing={8} width='80%'>
+						{motors.map((number) => (
+							<Box
+								padding='20px'
+								paddingTop='10px'
+								maxW='sm'
+								borderWidth='1px'
+								borderRadius='30px'>
+								<Text>Drink {number + 1}</Text>
+								<Flex>
+									<Button
+										className='drink-button'
+										colorScheme='blue'
+										size='lg'
+										height='60px'
+										width='40%'
+										onClick={() => send_motor_command(number, "on")}>
+										Start
+									</Button>
+									<Spacer />
+									<Button
+										className='drink-button'
+										colorScheme='blue'
+										size='lg'
+										height='60px'
+										width='40%'
+										onClick={() => send_motor_command(number, "off")}>
+										Stop
+									</Button>
+								</Flex>
+							</Box>
+						))}
+					</Stack>
+				</ChakraProvider>
+			</header>
+		</div>
+	);
+};
 
 export default App;
